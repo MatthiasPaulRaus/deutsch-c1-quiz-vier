3f261bec2da099f9d9b3bc558c0f2c4da104a78f
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-06-14 17:59:04 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Wenn ich lange sitze, tun mir die Beine weh.",
+        answersList: [
+            {answer:"Ja, beim Fliegen passiert mir das auch immer.", isCorrect:true},
+            {answer:"Ja, im Flugzeug kann man lange sitzen.", isCorrect:false},
+            {answer:"Ja, im Sitzen spüre ich meine Beine.", isCorrect:false},
+        ]
+    },{
+        question:"Beim Essen durften wir nicht spielen.",
+        answersList: [
+            {answer:"Wir könnten danach etwas spielen.", isCorrect:false},
+            {answer:"Wir durften das auch nicht machen.", isCorrect:true},
+            {answer:"Wir mussten immer draußen spielen.", isCorrect:false},
+        ]
+    },{
+        question:"Er klingelte an der Haustür,",
+        answersList: [
+            {answer:"die sich dann schnell öffnet.", isCorrect:false},
+            {answer:"welche daraufhin aufgehen könnte.", isCorrect:false},
+            {answer:"woraufhin diese sich langsam öffnete.", isCorrect:true},
+        ]
+    },{
+        question:"Er sucht das Museum auf dem Stadtplan.",
+        answersList: [
+            {answer:"Währenddessen fragt sie die Passanten danach.", isCorrect:true},
+            {answer:"Während sie von den Passanten danach gefragt wird.", isCorrect:false},
+            {answer:"Danach fand sie es gleich um die Ecke.", isCorrect:false},
+        ]
+    },{
+        question:"Er hörte sie noch etwas flüstern.",
+        answersList: [
+            {answer:"Dann war er langsam eingeschlafen.", isCorrect:false},
+            {answer:"Daraufhin muss er eingeschlafen sein.", isCorrect:true},
+            {answer:"Anschließend müsste er ein bisschen schlafen.", isCorrect:false},
+        ]
+    },{
+        question:"Zwecks Terminvereinbarung rufen Sie bitte im Sekretariat an.",
+        answersList: [
+            {answer:"Um einen Termin beim Sekretär zu bekommen, soll ich anrufen?", isCorrect:false},
+            {answer:"Damit wir den Termin verschieben, rufen Sie mich an?", isCorrect:false},
+            {answer:"Zur Terminvereinbarung soll ich also im Sekretariat anrufen?", isCorrect:true},
+        ]
+    },{
+        question:"Zum Zwecke der Teilnahme müssen Sie sich im Kursbüro anmelden.",
+        answersList: [
+            {answer:"Für die Teilnahme am Kurs muss ich mich anmelden.", isCorrect:true},
+            {answer:"Ohne Ihre Teilnahme kann der Kurs nicht stattfinden.", isCorrect:false},
+            {answer:"Zur Anmeldung gingen Sie ins Kursbüro.", isCorrect:false},
+        ]
+    },{
+        question:"Beim Spazierengehen habe ich die besten Einfälle.",
+        answersList: [
+            {answer:"Als ich spazieren ging, hatte ich keine Ahnung davon.", isCorrect:false},
+            {answer:"Wenn ich spazieren gehe, kommen mir viele gute Ideen.", isCorrect:true},
+            {answer:"Während wir spazieren gehen, könnte uns etwas einfallen.", isCorrect:false},
+        ]
+    },{
+        question:"Ehe er den Diebstahl bemerkte, waren die Räuber schon verschwunden.",
+        answersList: [
+            {answer:"Bevor die Räuber verschwanden, hatte er den Diebstahl bemerkt.", isCorrect:false},
+            {answer:"Nachdem er den Diebstahl bemerkte, verschwanden die Räuber.", isCorrect:false},
+            {answer:"Die Räuber waren schon weg, als er den Diebstahl bemerkte.", isCorrect:true},
+        ]
+    },{
+        question:"Ich habe die Erzählung nicht als gedrucktes Buch gekauft,",
+        answersList: [
+            {answer:"sondern als Podcast.", isCorrect:true},
+            {answer:"aber ein Podcast wäre mir lieber.", isCorrect:false},
+            {answer:"weil es das nicht als Podcast gibt.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Manche Leute haben eine derartige Angst vor dem Zahnarzt,",
+        answersList: [
+            {answer:"dass sie den Besuch lange aufschieben.", isCorrect:true},
+            {answer:"weil sie ihn nicht kontrollieren können.", isCorrect:false},
+            {answer:"denn ein Termin ist schwer zu bekommen.", isCorrect:false},
+        ]
+    },{
+        question:"Kinder haben immer ein solches Gefühl für Gerechtigkeit,",
+        answersList: [
+            {answer:"weil sie dadurch viel für das Leben lernen.", isCorrect:false},
+            {answer:"dass sie alles ganz genau teilen.", isCorrect:true},
+            {answer:"obwohl sie manchmal sehr gerecht teilen.", isCorrect:false},
+        ]
+    },{
+        question:"Die Familie legt großen Wert auf Gemeinsamkeit.",
+        answersList: [
+            {answer:"Daher treffen sie sich äußerst selten.", isCorrect:false},
+            {answer:"Denn allein zu leben bedeutet ihnen sehr viel.", isCorrect:false},
+            {answer:"Infolgedessen verbringen sie alle Wochenenden zusammen.", isCorrect:true},
+        ]
+    },{
+        question:"Infolge starker Unwetter",
+        answersList: [
+            {answer:"musste die Veranstaltung abgebrochen werden.", isCorrect:true},
+            {answer:"kann das Seminar im Freien stattfinden.", isCorrect:false},
+            {answer:"werden wir das Treffen wohl besser absagen.", isCorrect:false},
+        ]
+    },{
+        question:"Infolge eines Missverständnisses",
+        answersList: [
+            {answer:"werden wir wohl nie den Weg finden.", isCorrect:false},
+            {answer:"haben wir uns in der U-Bahn verfahren.", isCorrect:true},
+            {answer:"sollten wir den anderen Weg nehmen.", isCorrect:false},
+        ]
+    },{
+        question:"In manchen Ländern ist man nicht an laute Nachbarn gewöhnt,",
+        answersList: [
+            {answer:"weil man sich nur leise unterhalten kann.", isCorrect:false},
+            {answer:"denn der Straßenlärm ist überaus lästig.", isCorrect:false},
+            {answer:"weswegen man sich leise unterhalten sollte.", isCorrect:true},
+        ]
+    },{
+        question:"Mithilfe neuester medizinischer Verfahren",
+        answersList: [
+            {answer:"konnte die Krankheit schnell diagnostiziert werden.", isCorrect:true},
+            {answer:"werden die Diagnosen immer ungenauer.", isCorrect:false},
+            {answer:"werden die Spezialisten infrage gestellt.", isCorrect:false},
+        ]
+    },{
+        question:"Indem Sie den Wortschatz wöchentlich wiederholen",
+        answersList: [
+            {answer:"bereiten Sie sich nur einmal auf die Prüfung vor.", isCorrect:false},
+            {answer:"können Sie sich gut auf die Prüfung vorbereiten.", isCorrect:true},
+            {answer:"konnten Sie eine gute Vorbereitung machen.", isCorrect:false},
+        ]
+    },{
+        question:"Eine vergleichbare Situation gibt es in meinem Heimatland.",
+        answersList: [
+            {answer:"Hier ist nämlich alles verschieden.", isCorrect:false},
+            {answer:"Bei uns hält die Lage keinem Vergleich stand.", isCorrect:false},
+            {answer:"Auch bei uns leben viele Menschen allein.", isCorrect:true},
+        ]
+    },{
+        question:"Ebenso wichtig wie regelmäßig den Arzt zu besuchen,",
+        answersList: [
+            {answer:"ist es eine gesunde Ernährung zu haben.", isCorrect:true},
+            {answer:"sich gesund zu ernähren ist besser.", isCorrect:false},
+            {answer:"sollten Sie auf die Gesundheit achten.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Wenn man die Risiken betrachtet ... ",
+        answersList: [
+            {answer:"Angesichts der Risiken ...", isCorrect:true},
+            {answer:"Im Falle von Risiken ...", isCorrect:false},
+            {answer:"Falls es Risiken geben sollte ...", isCorrect:false},
+        ]
+    },{
+        question:"Anlässlich des 20-jährigen Jubiläums ...",
+        answersList: [
+            {answer:"Weil wir schon 20 Jahre alt sind, ...", isCorrect:false},
+            {answer:"Weil sich das Jubiläum zum 20. Mal wiederholt, ...", isCorrect:true},
+            {answer:"Wenn Sie das Jubiläum feiern wollen, ...", isCorrect:false},
+        ]
+    },{
+        question:"Das Museum finden Sie gleich hier um die Ecke.",
+        answersList: [
+            {answer:"Unweit des Museums finden Sie, was Sie suchen.", isCorrect:false},
+            {answer:"In der Nähe des Museums sehen sie die Kreuzung.", isCorrect:false},
+            {answer:"Bis zum Museum ist es nicht mehr weit.", isCorrect:true},
+        ]
+    },{
+        question:"Hinsichtlich der genannten Gründe ...",
+        answersList: [
+            {answer:"In Bezug auf die gegebenen Gründe ...", isCorrect:true},
+            {answer:"Mit Blick auf die zu nennenden Gründe ...", isCorrect:false},
+            {answer:"Angesichts Ihrer gegebenen Begründung ...", isCorrect:false},
+        ]
+    },{
+        question:"Die Teilnehmer, großem Stress ausgesetzt, haben keine Hilfsmittel bei der Prüfung.",
+        answersList: [
+            {answer:"Während der Prüfung dürfen sie nicht um Hilfe bitten.", isCorrect:false},
+            {answer:"Solange die Prüfung stattfindet sollten Sie Ruhe bewahren.", isCorrect:false},
+            {answer:"Auch bei Stress dürfen die Teilnehmer keine Wörterbücher benutzen.", isCorrect:true},
+        ]
+    },{
+        question:"Gemütlich auf dem Sofa liegend kann man den Abend genießen.",
+        answersList: [
+            {answer:"Während Sie auf dem Sofa liegen, können Sie den Abend genießen.", isCorrect:true},
+            {answer:"Wenn Sie auf dem Sofa liegen, ist der Abend besonders schön.", isCorrect:false},
+            {answer:"Immer wenn Sie auf dem Sofa liegen, ist der Abend zu genießen.", isCorrect:false},
+        ]
+    },{
+        question:"Schon nach kurzer Zeit hatte ich seine Absichten durchschaut.",
+        answersList: [
+            {answer:"Was er tun wollte, war mir völlig undurchschaubar.", isCorrect:false},
+            {answer:"Ich habe gleich gemerkt, was er vorhatte.", isCorrect:true},
+            {answer:"Sein Vorhaben war mir von Anfang an zweifelhaft.", isCorrect:false},
+        ]
+    },{
+        question:"Ich unterstelle Ihnen keine bösartigen Absichten.",
+        answersList: [
+            {answer:"Ich kann nicht glauben, was Sie da beabsichtigen.", isCorrect:false},
+            {answer:"Es fällt mir schwer, Ihre Absichten zu durchschauen.", isCorrect:false},
+            {answer:"Ich glaube Ihnen, dass Sie nichts Schlechtes vorhaben.", isCorrect:true},
+        ]
+    },{
+        question:"Aufgrund eines Staus sollten Sie das Zentrum weitgehend umfahren.",
+        answersList: [
+            {answer:"Fahren Sie nicht in die Stadtmitte.", isCorrect:true},
+            {answer:"Fahren Sie langsam um die Stadt herum.", isCorrect:false},
+            {answer:"Wegen dem Stau müssen Sie länger warten.", isCorrect:false},
+        ]
+    },{
+        question:"Der Roman spielt in einer Großstadt.",
+        answersList: [
+            {answer:"Die Nebenfiguren lebten auf dem Land.", isCorrect:false},
+            {answer:"Zur Einleitung gehören die Probleme der Gegenspieler.", isCorrect:false},
+            {answer:"Die Protagonisten zeichnen sich dadurch aus, dass ...", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
